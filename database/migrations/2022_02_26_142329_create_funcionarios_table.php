fe966e70d5e45a505f56bb307db33101be64c3ce
Camila Souza <camilamaiara@hotmail.com.br>
2022-02-26 11:40:02 -0300

    Criando migrations e models

---
+++ database/migrations/2022_02_26_142329_create_funcionarios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFuncionariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cpf')->unique();
+            $table->string('nome');
+            $table->string('carteira_trabalho');
+            $table->integer('setor_id')->unsigned();
+            $table->integer('telefone_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->foreign('telefone_id')->references('id')->on('telefones');
+            $table->foreign('setor_id')->references('id')->on('setores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+}
